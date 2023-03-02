3a0a509b61ae8a98773c0c51bae0ad62a26d6b21
Marxho <marxhop@hotmail.com>
2023-03-02 13:15:50 -0500

    added array with questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -2,8 +2,73 @@
 const inquirer = require('inquirer');
 const fs = require('fs')
 const markdownGenerator = require('./generateMarkdown')
-// TODO: Create an array of questions for user input
-const questions = [];
+
+// Questions for user input
+const questions = [
+    // Project's name
+    {
+      type: "input",
+      name: "title",
+      message: "Please provide your project's title",
+    },
+    // Project's description
+    {
+      type: "input",
+      name: "description",
+      message: "Decribe your project",
+    },
+    // Instructions for installation
+    {
+      type: "input",
+      name: "installation",
+      message: "What are the installation procedures?",
+    },
+    // Usage information
+    {
+      type: "input",
+      name: "usage",
+      message: "How do you use this project? ",
+    },
+    // Contribution guidelines
+    {
+      type: "input",
+      name: "contribution",
+      message: "How can other developers contribute to this project?",
+    },
+    // Test Instructions
+    {
+      type: "input",
+      name: "testing",
+      message: "What are the testing procedures?",
+    },
+    // License Options
+    {
+      type: "checkbox",
+      name: "licensing",
+      message: "Choose a license for your project.",
+      options: [   
+        "BSD 3-Clause",
+        "GNU v3",
+        "Mozilla Public License 2.0",
+        "Apache 2.0",
+        "MIT",
+        "Unlicense",
+      ],
+    },
+    // Github Username
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+     
+    },
+    // Email Address
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address?",
+    }
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
